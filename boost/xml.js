ecf1fd2b408908b2128656604ac312a1a8e61411
lvsheng <lvsheng@baidu.com>
2015-09-14 22:01:20 +0800

    merge xmlParser.js -> xml.js

--- boost/xml.js
+++ boost/xml.js
@@ -1,230 +1,198 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var derive = require("base/derive");
-    var assert = require("base/assert");
-    var trim = require("base/trim");
-    var each = require("base/each");
-    var copyProperties = require("base/copyProperties");
     var boost = require("boost/boost");
-    var Text = require("boost/Text");
-    var TextInput = require("boost/TextInput");
+    var StyleRender = require("boost/styleRender");
     var EventTarget = require("boost/EventTarget");
-    var Event = require("boost/Event");
-    var nativeGlobal = require("boost/NativeObject").global;
-    var toCamelCase = require("base/toCamelCase");
+    var copyProperties = require("base/copyProperties");
     var webMap = require("boost/webMap");
-    var webDebugger = require('./webDebugger');
+    var webDebugger = require("boost/webDebugger");
+    var nativeGlobal = require("boost/NativeObject").global;
+    var FROM_CUSTOM_HANDLER = "__from_custom_handler__";
 
-    function onStateChanged() {
-        if (this.readyState == 4) {
-            loadFromString(this.responseText);
-        }
+    /**
+     * @param [customHandler] {Function} TODO: change to webComponent/customElement
+     * @constructor
+     */
+    function XmlParser (customHandler) {
+        this._customHandler = customHandler;
+        this._styleRender = new StyleRender(this); //每次实例一个以避免本次parse中的style影响外界
     }
+    XmlParser.prototype = {
+        /**
+         * @param xmlStr {string} 不需包含<?xml>说明节点，需为单根
+         * @return {Element}
+         */
+        parse: function (xmlStr) {
+            var domParser = new DOMParser();
+            var XML_DECLARE_STR = "<?xml version='1.0' encoding='UTF-8' ?>";
+            var xmlDoc = domParser.parseFromString(XML_DECLARE_STR + xmlStr, "text/xml");
+            return this._process(xmlDoc);
+        },
 
-    function loadFromURL(url) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = onStateChanged;
-        xhr.open("GET", url, true);
-        xhr.send(null);
-    }
-
-
-    function loadFromString(str) {
-        var parser = new DOMParser();
-        var xmlDoc = parser.parseFromString(str, "text/xml");
-        process(xmlDoc);
-    }
-
-    function process(document) {
-        console.log("process:", document);
-        walkElement(document.documentElement, boost.documentElement);
-        applyStyle();
-        var event = new Event(xml, "domready");
-        xml.dispatchEvent(event);
-    }
-
-
-    function processElement(element, nativeParentElement) {
-        var nodeType;
-        var str;
-        var nativeElement;
-        var attributes;
-        var attribute;
-        var count;
-        var index;
-
-
-        var tagName = element.tagName.toUpperCase();
-        switch (tagName) {
-        case "PARSERERROR":
-            console.error(element.innerText);
-            break;
-        case "STYLE":
-            parseStyle(element.firstChild.nodeValue);
-            break;
-        case "FLUSH":
-            nativeGlobal.test();
-            break;
-        default:
-            nativeElement = boost.createElement(element.tagName);
-            nativeParentElement.appendChild(nativeElement);
-            attributes = element.attributes;
-            count = attributes.length;
-            for (index = 0; index < count; index++) {
-                attribute = attributes[index];
-                nativeElement.setAttribute(attribute.name, attribute.value);
-            }
-
-            if (tagName === "TEXT" || tagName === "TEXTINPUT") {
-                if (element.firstChild !== null) {
-                    nativeElement.value = element.firstChild.nodeValue;
-
-                    if (webDebugger.isActive()) {
-                        webMap.getWebElement(nativeElement).innerText = element.firstChild.nodeValue;
-                    }
-                }
-            } else {
-                walkElement(element, nativeElement);
-            }
-        }
-    }
-
-    function walkElement(element, nativeParentElement) {
-        var child;
-        var tag;
-        for (child = element.firstElementChild; child !== null; child = child.nextElementSibling) {
-            processElement(child, nativeParentElement);
-        }
-    }
-
-    var parser = null;
-    var ruleList = [];
-
-    function parseStyle(str) {
-        if (parser === null) {
-            parser = new StyleParser();
-        }
-        parser.parse(str, ruleList);
-    }
-
-
-    function applyStyle() {
-        each(ruleList, function (item) {
-            var selector = item.selector;
-            var elements = boost.querySelectorAll(selector);
-            each(elements, function (element) {
-                copyProperties(element.style, item.rule);
+        /**
+         * 判断一个元素是否来自this._customHandler（而非parse中直接处理到的默认标签）
+         * @param element
+         * @param rootElement
+         * @returns {boolean}
+         */
+        fromCustomHandler: function (element, rootElement) {
+            var elementWithMark = this._findUpstream(element, function (node) {
+                return node.getAttribute(FROM_CUSTOM_HANDLER);
             });
-        });
-    }
 
-    var StyleParser = derive(Object, function () {
-        var _code = null;
-        var _codeChar = null;
-        var _index = 0;
-        var _count = 0;
-    }, {
-        parse: function (str, ret) {
-            this._code = this.preProcess(str);
-            this._codeChar = this._code.split("");
-            this._index = 0;
-            this._count = this._codeChar.length;
-            var selector;
-            var rule;
+            return !!(elementWithMark && elementWithMark !== rootElement);
+        },
 
-            ret = ret || [];
-            while (true) {
-                this.ignoreWhiteSpace();
-                selector = this.getSelector();
-                rule = this.getRule();
-                if (selector && rule) {
-                    ret.push({
-                        selector: selector,
-                        rule: rule
-                    });
-                    continue;
+        /**
+         * 从node开始，向上寻找
+         * @param node {Element}
+         * @param predicate {Function}
+         * @return {Element|null}
+         * @private
+         */
+        _findUpstream: function (node, predicate) {
+            do {
+                if (predicate(node) === true) {
+                    return node;
                 }
-                break;
+            } while ((node = node.parentNode) !== null);
+            return null;
+        },
+
+        /**
+         * @param xmlDocument
+         * @return {Element}
+         * @private
+         */
+        _process: function (xmlDocument) {
+            console.log("process:", xmlDocument);
+            if (xmlDocument.querySelector('parsererror div')) {
+                console.error("xml parse error: " + xmlDocument.querySelector('parsererror div').innerText);
             }
-            return ret;
+
+            var rootNativeElement = this._processElement(xmlDocument.documentElement);
+            this._styleRender.apply(rootNativeElement);
+            return rootNativeElement;
         },
-        preProcess: function (str) {
-            return str.replace(/\/\*[\s\S]*?\*\//g, "");
-        },
-        ignoreWhiteSpace: function () {
-            var count = this._count;
-            var _codeChar = this._codeChar;
-            var code;
+
+        /**
+         * @param xmlElement
+         * @returns {Element|null}
+         * @private
+         */
+        _processElement: function (xmlElement) {
+            var nativeElement;
+            var attributes;
+            var attribute;
+            var count;
             var index;
 
-            for (index = this._index; index < count; index++) {
-                code = _codeChar[index];
-                if (code !== " " && code !== "\t" && code !== "\r" && code !== "\n") {
+            var result = null;
+            var tagName = xmlElement.tagName.toUpperCase();
+            switch (tagName) {
+                case "PARSERERROR":
+                    console.error(element.innerText);
                     break;
+
+                case "STYLE":
+                    this._styleRender.parse(xmlElement.firstChild ? xmlElement.firstChild.nodeValue: '');
+                    break;
+
+                case "FLUSH":
+                    nativeGlobal.test();
+                    break;
+
+                default:
+                    var customHandlerResult = this._customHandler ? this._customHandler(xmlElement) : false;
+                    switch (customHandlerResult) {
+                        case false:
+                            //this._customHandler未处理，走默认处理
+                            nativeElement = boost.createElement(xmlElement.tagName);
+                            attributes = xmlElement.attributes;
+                            count = attributes.length;
+                            for (index = 0; index < count; index++) {
+                                attribute = attributes[index];
+                                nativeElement.setAttribute(attribute.name, attribute.value);
+                            }
+
+                            if (tagName === "TEXT" || tagName === "TEXTINPUT") {
+                                var value = xmlElement.firstChild ? xmlElement.firstChild.nodeValue : '';
+                                nativeElement.value = value;
+
+                                if (webDebugger.isActive()) {
+                                    webDebugger.ignoreWebChange = true;
+                                    webMap.getWebElement(nativeElement).innerText = value;
+                                    webDebugger.ignoreWebChange = false;
+                                }
+                            } else {
+                                this._walkElement(xmlElement, nativeElement);
+                            }
+
+                            //TODO: 在Element定义属性
+                            nativeElement.__styleRender__ = this._styleRender;
+
+                            result = nativeElement;
+                            break;
+
+                        case undefined:
+                        case null:
+                            //handler处理但没有dom元素要添加
+                            break;
+
+                        default :
+                            //handler处理并返回其处理结果，打标记并直接append
+                            customHandlerResult.setAttribute(FROM_CUSTOM_HANDLER, true);
+                            result = customHandlerResult;
+                            break;
+                    }
+            }
+
+            return result;
+        },
+
+        _walkElement: function (xmlElement, nativeParentElement) {
+            var xmlChild;
+            for (xmlChild = xmlElement.firstElementChild; xmlChild !== null; xmlChild = xmlChild.nextElementSibling) {
+                var elementChild = this._processElement(xmlChild);
+                if (elementChild) {
+                    nativeParentElement.appendChild(elementChild);
                 }
             }
-            this._index = index;
-        },
-        getSelector: function () {
-            var code = this._code;
-            var end = code.indexOf("{", this._index);
-            var selector;
-            if (end < 0) {
-                this.end();
-                return false;
-            }
-            selector = code.substring(this._index, end);
-            this._index = end;
-            return trim(selector);
-        },
-        getRule: function () {
-            var code = this._code;
-            var index = this._index;
-            var start = code.indexOf("{", index);
-            var end;
-
-            if (start < 0) {
-                this.end();
-                return false;
-            }
-
-            end = code.indexOf("}", start);
-            if (end < 0) {
-                this.end();
-                return false;
-            }
-
-            this._index = end + 1;
-            return this.parseRule(code.substring(start + 1, end));
-        },
-        parseRule: function (str) {
-            var list = str.split(";");
-            var count = list.length;
-            var index;
-            var item;
-            var parts;
-            var key;
-            var ret = {};
-
-            for (index = 0; index < count; index++) {
-                item = list[index];
-                parts = item.split(":");
-                if (parts.length > 1) {
-                    key = toCamelCase(trim(parts[0]));
-                    ret[key] = trim(parts[1]);
-                }
-            }
-            return ret;
-        },
-        end: function () {
-            this._index = this._count;
         }
-    });
+    };
 
     var xml = new EventTarget();
-    xml.loadFromURL = loadFromURL;
-    xml.loadFromString = loadFromString;
+    copyProperties(xml, {
+        /**
+         * @param xmlStr {string} xml串，不需包含<?xml>说明标签，需单根
+         * @param [customHandler] {Function}
+         * @returns {*|Element}
+         */
+        parse: function (xmlStr, customHandler) {
+            var parser = new XmlParser(customHandler);
+            return parser.parse(xmlStr);
+        },
 
+        loadFromURL: function (url) {
+            var self = this;
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function onStateChanged() {
+                if (this.readyState == 4) {
+                    self.loadFromString(this.responseText);
+                }
+            };
+            xhr.open("GET", url, true);
+            xhr.send(null);
+        },
+
+        loadFromString: function (str) {
+            var element = this.parse(str);
+            //TODO: remove <?xml>?
+            boost.documentElement.appendChild(element);
+            var event = new Event(xml, "domready");
+            xml.dispatchEvent(event);
+        }
+    });
     module.exports = xml;
 });
