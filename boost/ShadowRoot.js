aad44101d1b11f84813619c2a1bb497e4bcf4188
lvsheng <lvsheng@baidu.com>
2015-10-08 21:31:32 +0800

    解决循环依赖，能渲染非shadowDom元素

--- boost/ShadowRoot.js
+++ boost/ShadowRoot.js
@@ -1,17 +1,24 @@
 define(function (require, exports, module) {
     var derive = require("base/derive");
-    var Element = require("boost/Element"); //TODO: 解决循环依赖的问题~
+    require("boost/Element");
 
-    /**
-     * shadowRoot不参与渲染，故只继承Element(w3c规范里是继承自DocumentFragment、又增加了innerHTML与styleSheets等属性)
-     */
-    module.exports = derive(Element, function (host) {
-        this._super("ShadowRoot");
+    var ShadowRoot;
+    exports.getShadowRoot = function () {
+        //FIXME: 因与Element有循环依赖，故暂时封一个方法而非直接exports
+        if (!ShadowRoot) {
+            /**
+             * shadowRoot不参与渲染，故只继承Element(w3c规范里是继承自DocumentFragment、又增加了innerHTML与styleSheets等属性)
+             */
+            ShadowRoot = derive(require("boost/Element"), function (host) {
+                this._super("ShadowRoot");
 
-        this.__host__ = host;
-    }, {
-        "get host": function () {
-            return this.__host__;
+                this.__host__ = host;
+            }, {
+                "get host": function () {
+                    return this.__host__;
+                }
+            });
         }
-    });
+        return ShadowRoot;
+    };
 });
