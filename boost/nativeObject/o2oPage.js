fa4c9fe50461dd0ee84b03278cee0ec1682dc509
lvsheng <lvsheng@baidu.com>
2015-10-12 09:48:19 +0800

    增加o2oPage的订阅相关接口

--- boost/nativeObject/o2oPage.js
+++ boost/nativeObject/o2oPage.js
@@ -1,13 +1,13 @@
 define(function (require, exports, module) {
     "use strict";
 
-    //TODO: 限制只有背景页可以使用？
     var derive = require("base/derive");
     var assert = require("base/assert");
     var NativeObject = require("boost/nativeObject/NativeObject");
 
     var O2OPage_TYPE_ID = -3;
     /**
+     * 只能在背景页中使用，前景页无法使用
      * 供背景页用作与前景页通信的中介（向前景页发送消息，监听前景页发来的消息）
      */
     var O2OPageNativeObject = derive(NativeObject, function () {
@@ -18,6 +18,26 @@ define(function (require, exports, module) {
         },
         attachToPopWindow: function (menuNativeObjectId) {
             this.__callNative("attachToPopWindow", [menuNativeObjectId]);
+        },
+        follow: function (conf) {
+            this.__callNative("subscribeLight", [conf.appid, 'clouda', false]); //TODO: conf.is_silence参数的支持？
+        },
+        unfollow: function (conf) {
+            this.__callNative("unsubscribeLight", [conf.appid, 'clouda']);
+        },
+        checkFollow: function (conf) {
+            this.__callNative("isSubscribeLight", [conf.appid]);
+        },
+        share: function (conf) {
+            //options = {
+            //    appid: appId,
+            //    mediaType: 'all',
+            //    title: title,
+            //    content: content,
+            //    linkUrl: url,
+            //    imageUrl: iconUrl
+            //}
+            this.__callNative("shareApp", [conf]); //TODO: 参数怎么传？
         }
     });
 
