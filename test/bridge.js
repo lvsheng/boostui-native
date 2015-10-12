050578a3b8e94c3d6ec4ca3cf96d10e2c417c01f
lvsheng <lvsheng@baidu.com>
2015-10-12 10:36:20 +0800

    update test/bridge.js

--- test/bridge.js
+++ test/bridge.js
@@ -1,35 +1,9 @@
 "use strict";
-(function (window) {
-    var OBJECT_NAME = 'lc_bridge';
-    var METHOD_LIST = ['callQueue'];
-    var INJECT_PREFIX = 'O2OZone#234asdg233hfr90';
-
-    var slice = [].slice;
-    var prompt = window.prompt;
-
-    var injectObj = window[OBJECT_NAME] = window[OBJECT_NAME] || {};
-    inject(injectObj, OBJECT_NAME, METHOD_LIST);
-
-    function inject(obj, name, methods) {
-        obj = obj || {};
-        var count = methods.length;
-        var index;
-        var key;
-
-        for (index = 0; index < count; index++) {
-            key = methods[index];
-            obj[key] = bindNative(name, key);
-        }
-        return obj;
-    }
-
-    function bindNative(name, method) {
-        return function () {
-            var methodStr = JSON.stringify([name, method]);
-            //console.info(+new Date);
-            window.console.log(INJECT_PREFIX + ':' + methodStr.length + ':'
-                + methodStr
-                + JSON.stringify(slice.call(arguments)));
-        };
-    }
+(function(window) {
+    var INJECT_PREFIX = 'O2OZone#YIFMWBUIQ98S38FR:';
+    var lc_bridge = window.lc_bridge = window.lc_bridge || {};
+    var console = window.console;
+    lc_bridge.callQueue = function(list) {
+        console.log(INJECT_PREFIX + JSON.stringify(list));
+    };
 })(window);
