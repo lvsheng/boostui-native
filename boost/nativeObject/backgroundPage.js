87d24d635f1e3b7951bf166736def77010aa22c0
lvsheng <lvsheng@baidu.com>
2015-12-22 15:26:48 +0800

    评论与咨询改为新页面打开

--- boost/nativeObject/backgroundPage.js
+++ boost/nativeObject/backgroundPage.js
@@ -7,7 +7,7 @@ define(function (require, exports, module) {
     var bridge = require("boost/bridge");
     var NativeObject = require("boost/nativeObject/NativeObject");
     var Event = require("boost/Event");
-
+    var mainFrontPage = require("boost/mainFrontPage");
     var BACKGROUND_PAGE_TYPE_ID = -1;
     /**
      * 供前景页用作与背景页通信的中介（向背景页发送消息，监听背景页发来的消息）
@@ -29,6 +29,34 @@ define(function (require, exports, module) {
             }
             return event && event.propagationStoped;
         },
+
+        /**
+         * @param url
+         * @param [useMiddlePage] {boolean}
+         * @param [tipText]
+         * @param [delay] {number} 毫秒为单位
+         */
+        openNewPage: function (url, useMiddlePage, tipText, delay) {
+            var data = {
+                url: completeUrl(url),
+                useMiddlePage: useMiddlePage,
+                tipText: tipText,
+                delay: delay
+            };
+            this.postMessage("openPage", data);
+        },
+        openNewPageFromBg: function (url) {
+            assert(/^https?:\/\//.test(url), "背景页打开新页面，必需传入完整url");
+            mainFrontPage.dispatchEvent({
+                type: "message",
+                data: {
+                    action: "openPage",
+                    data: {
+                        url: url
+                    }
+                }
+            });
+        },
         postMessage: function (action, data) {
             bridge.postMessage({
                 action: action,
@@ -44,4 +72,17 @@ define(function (require, exports, module) {
     });
 
     module.exports = new BackgroundPageNativeObject();
+
+    function completeUrl (inValue) {
+        if (/^https?:\/\//.test(inValue)) {
+            return inValue;
+        }
+
+        if (inValue[0] === "/") {
+            return location.origin + inValue;
+        }
+
+        var h = location.href;
+        return h.slice(0, h.lastIndexOf("/")) + "/" + inValue.slice(1);
+    }
 });
