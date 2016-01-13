e1cc8c1a1ab795c71000fd5faa59d9f77e1dfab9
lvsheng <lvsheng@baidu.com>
2016-01-13 11:33:43 +0800

    Linkage内屏蔽color

--- boost/nativeObject/Linkage.js
+++ boost/nativeObject/Linkage.js
@@ -6,20 +6,18 @@ define(function (require, exports, module) {
     var each = require("base/each");
     var copyProperties = require("base/copyProperties");
     var NativeElement = require("boost/NativeElement");
+    var validator = require("boost/validator");
     var NativeObject = require("boost/nativeObject/NativeObject");
     var TYPE_ID = require("boost/TYPE_ID");
 
     /**
      * 联动器
-     * @param conf.prop {string}
-     * @param conf.from {number}
-     * @param conf.to {number}
+     * @param conf.prop {string} 支持各数值属性与颜色属性（color|backgroundColor）
+     * @param conf.from {number | color}
+     * @param conf.to {number | color}
      * @param conf.target {NativeElement}
      * @param conf.duration {number}
      * @param conf.type {"together"|"sequentially"}
-     * @param conf.fromColor {int} 应为validator.color返回值 TODO: 本方法内做color的转换
-     * @param conf.toColor {int}
-     * @param conf.updateColor {boolean}
      */
     var LinkageNativeObject = derive(NativeObject, function (conf) {
         conf = copyProperties({}, conf);
@@ -27,6 +25,17 @@ define(function (require, exports, module) {
             assert(conf.target instanceof NativeElement);
             conf.target = conf.target.nativeObject.tag;
         }
+
+        var colorProps = ["color", "backgroundColor"];
+        if (colorProps.indexOf(conf.prop) > -1) {
+            //isColor
+            conf.updateColor = true;
+            conf.fromColor = validator.color(conf.from);
+            conf.toColor = validator.color(conf.to);
+            delete conf.from;
+            delete conf.to;
+        }
+
         NativeObject.call(this, TYPE_ID.LINKAGE, undefined, conf);
 
         this._curIndex = 0;
